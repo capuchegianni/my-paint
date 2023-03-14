364282ca4683b6e3b00877f786bd4478ea3617fd
ElouanR <elouan.rigomont@epitech.eu>
2023-03-14 14:43:02 +0100

    [+] save, open, new file + basic draw

--- src/main.c
+++ src/main.c
@@ -8,6 +8,7 @@
 #include "../include/header.h"
 
 sfRenderWindow *window;
+draw_area_t *area;
 
 void event_actions(sfRenderWindow *window)
 {
@@ -25,8 +26,6 @@ void init_window(void)
 {
     window = sfRenderWindow_create((sfVideoMode)
     {1920, 1080, 32}, "my_paint", sfClose | sfResize, NULL);
-
-    sfRenderWindow_setFramerateLimit(window, 60);
 }
 
 void init_menu(global_filemenu_t **global_filemenu,
@@ -53,7 +52,7 @@ global_editionmenu_t **global_editionmenu, global_helpmenu_t **global_helpmenu)
     *global_helpmenu = global_helpmenu_cp;
 }
 
-void start_window(void)
+int start_window(char **av)
 {
     global_filemenu_t *global_filemenu = malloc(sizeof(global_filemenu_t));
     global_editionmenu_t *global_editionmenu =
@@ -61,20 +60,19 @@ void start_window(void)
     global_helpmenu_t *global_helpmenu = malloc(sizeof(global_helpmenu_t));
     init_window();
     init_menu(&global_filemenu, &global_editionmenu, &global_helpmenu);
+    if (init_area(av) == 84)
+        return (84);
 
     while (sfRenderWindow_isOpen(window)) {
         sfRenderWindow_clear(window, sfBlack);
-        hover_filemenu(global_filemenu->filemenu, global_filemenu->newfile,
-        global_filemenu->openfile, global_filemenu->savefile);
-        hover_editionmenu(global_editionmenu->editionmenu,
-        global_editionmenu->pencil, global_editionmenu->eraser);
-        hover_helpmenu(global_helpmenu->helpmenu,
-        global_helpmenu->about, global_helpmenu->help);
-        display_menu(window, global_filemenu->filemenu,
-        global_editionmenu->editionmenu, global_helpmenu->helpmenu);
+        display_menu(window, global_filemenu, global_editionmenu,
+        global_helpmenu);
+        sfRenderWindow_drawSprite(window, area->sprite, NULL);
+        draw();
         sfRenderWindow_display(window);
         event_actions(window);
     }
+    return (0);
 }
 
 int main(int ac, char **av)
@@ -82,5 +80,5 @@ int main(int ac, char **av)
     if (ac != 1 && !av[0])
         return (84);
 
-    start_window();
+    return (start_window(av));
 }
