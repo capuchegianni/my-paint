97c0eea3b212ff24261b97e5a9f8b3e8fd704112
Gianni Henriques <gianni.henriques@epitech.eu>
2023-03-16 16:50:52 +0100

    [+] finished help (look at the bonus pls)

--- src/menu/help/help_text.c
+++ src/menu/help/help_text.c
@@ -21,16 +21,69 @@ void add_heart(void)
     sfTexture_destroy(love);
 }
 
+void add_important(void)
+{
+    sfTexture *important = sfTexture_createFromFile("assets/images/bonus.png",NULL);
+    sfSprite *important_sprite = sfSprite_create();
+    sfVector2f important_pos = {120, 500};
+
+    sfSprite_setScale(important_sprite, (sfVector2f){0.1, 0.1});
+    sfSprite_setTexture(important_sprite, important, sfTrue);
+    sfSprite_setPosition(important_sprite, important_pos);
+    sfRenderWindow_drawSprite(window, important_sprite, NULL);
+    sfSprite_destroy(important_sprite);
+    sfTexture_destroy(important);
+}
+
+void bonus_help(void)
+{
+    char *bucket = "- The bucket button is available in the edition menu. \
+To fill the canvas with a color, you need to click on the left button of \
+your mouse while being above the canvas.";
+    char *colors = "- You can pick a color directly on the canvas by \
+right clicking on the pixel where the color you want is.";
+
+    my_print_text(window, 30, (sfVector2f){10, 510}, "Bonus:");
+    add_important();
+    my_print_text(window, 20, (sfVector2f){20, 550}, bucket);
+    my_print_text(window, 20, (sfVector2f){20, 580}, colors);
+}
+
+void file_help(void)
+{
+    char *file = "- To create a new file, click on the newfile button \
+in the file menu.";
+    char *open = "- The button openfile in the file menu doesn't work yet. \
+You can instead call a file in the terminal by typing: ./my_paint [file_name]";
+    char *save = "- To save your file, click on the savefile button in the \
+file menu.";
+
+    my_print_text(window, 30, (sfVector2f){10, 360}, "File menu:");
+    my_print_text(window, 20, (sfVector2f){20, 400}, file);
+    my_print_text(window, 20, (sfVector2f){20, 430}, open);
+    my_print_text(window, 20, (sfVector2f){20, 460}, save);
+}
+
 void base_prints(void)
 {
     char *welcome = "Thank you for using our paint program!";
-    char *big_str = "To be able to draw, you need to click on the left button \
-of your mouse while being above the canvas.\nYou can select either a pencil \
-or an eraser in the edition menu.\nTo change the color of your pencil, you \
-have to click on a colored square in the left menu.\nYou can also change the \
-size of your pencil by clicking on the size button in the left menu.\n";
-
+    char *draw = "- To draw, you need to click on the left button \
+of your mouse while being above the canvas.";
+    char *erase = "- The eraser button is available in the edition menu. \
+To erase, you need to click on the left button of your mouse while being \
+above the canvas.";
+    char *colors = "- You can pick a color by left clicking on the color \
+you want in the left menu.";
+    char *size = "- Pencil/eraser size is available in the left menu.";
+    char *shape = "- Pencil/eraser shape is available in the left menu.";
     my_print_text(window, 50, (sfVector2f){10, 60}, welcome);
-    my_print_text(window, 20, (sfVector2f){10, 130}, big_str);
     add_heart();
+    my_print_text(window, 30, (sfVector2f){10, 150}, "Basis:");
+    my_print_text(window, 20, (sfVector2f){20, 190}, draw);
+    my_print_text(window, 20, (sfVector2f){20, 220}, erase);
+    my_print_text(window, 20, (sfVector2f){20, 250}, colors);
+    my_print_text(window, 20, (sfVector2f){20, 280}, size);
+    my_print_text(window, 20, (sfVector2f){20, 310}, shape);
+    file_help();
+    bonus_help();
 }
